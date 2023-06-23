a3e55c966ed4b182bb7c3fca2721f09839b3bdf9
labSoft1 <luis.pilamunga@epn.edu.ec>
2023-06-23 09:22:48 -0500

    test1

---
+++ RestauranteFM/src/main/java/ec/edu/epn/RestauranteFM/RestauranteFmApplication.java
@@ -0,0 +1,14 @@
+package ec.edu.epn.RestauranteFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteFmApplication {
+
+	public static void main(String[] args) {
+		System.out.println("inicia");
+		SpringApplication.run(RestauranteFmApplication.class, args);
+	}
+
+}
---
+++ RestauranteFM/src/test/java/ec/edu/epn/RestauranteFM/RestauranteFmApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.epn.RestauranteFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteFmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
